556dfa5fc647535ba33ef249e0ba0e73b3837fb7
michael hohl <mhohl@rocketmail.com>
2018-03-22 19:15:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,15 @@ class Song
   end
 
   def self.find_by_name(name)
-    Song.all.detect { |x| x.name == name}
+    Song.all.detect { |x| x.name == name }
   end
 
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 
 end
