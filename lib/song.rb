18bd021a0c0aef8611c14b920c0edb7c38fd8c81
michael hohl <mhohl@rocketmail.com>
2018-03-22 19:04:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,21 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new 
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create 
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect { |x| x.name == name}
+  end
+
+
 end
